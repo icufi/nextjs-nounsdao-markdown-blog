a11d382428d84c9cd0d7f7fc60afbec8ec05dfb2
Max Girteit <icufishmg@gmail.com>
2022-06-27 13:25:47 -0600

    added noun logo image to logo comp

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import Image from 'next/Image';
+
+import classes from './logo.module.css';
 
 const Logo = () => {
   return (
-    <div className="">Logo</div>
-  )
-}
+    <div className={classes.logo}>
+      <Image
+        src='/images/site/Noun_Glasses.png'
+        alt='Noun glasses'
+        width={300}
+        height={300}
+      />
+    </div>
+  );
+};
 
 export default Logo;
