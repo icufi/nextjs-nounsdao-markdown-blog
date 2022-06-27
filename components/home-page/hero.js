a6d3bfd9bdc359482aa7366c486a965a2e8776db
Max Girteit <icufishmg@gmail.com>
2022-06-27 12:41:20 -0600

    added hero image and intro content

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,5 +1,19 @@
+import Image from 'next/image'
+
+import classes from './hero.module.css';
+
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className={classes.hero}>
+
+<div className={classes.image}>
+  <Image src="/images/site/foxy_noun.png" alt="An image showing a Noun NFT." width={300} height={300} />
+</div>
+<h1>Hi, I'm a Noun.</h1>
+<p>I blog about the comings and goings of the Nouns DAO. Oh!.. and value creation with on-chain communities too.</p>
+    </section>
+  )
 };
 
 export default Hero;
