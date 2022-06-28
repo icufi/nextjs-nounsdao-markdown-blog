a1416f53227bee4d5015a95e4dd178530220f858
Max Girteit <icufishmg@gmail.com>
2022-06-28 09:33:44 -0600

    test post data added

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,53 @@
-import { Fragment} from 'react';
+import { Fragment } from 'react';
 
 import Hero from '../components/home-page/Hero';
 import FeaturedPosts from '../components/home-page/featured-posts';
 
+const TEST_POSTS = [
+  {
+    slug: 'this-is-a-test',
+    title: 'Test Title',
+    image: 'test-image.png',
+    excerpt: 'This is a test of a test of a test blog.',
+    date: '2022-06-28',
+  },
+  {
+    slug: 'this-is-a-test2',
+    title: 'Test Title',
+    image: 'test-image.png',
+    excerpt: 'This is a test of a test of a test blog.',
+    date: '2022-06-28',
+  },
+  {
+    slug: 'this-is-a-test3',
+    title: 'Test Title',
+    image: 'test-image.png',
+    excerpt: 'This is a test of a test of a test blog.',
+    date: '2022-06-28',
+  },
+  {
+    slug: 'this-is-a-test4',
+    title: 'Test Title',
+    image: 'test-image.png',
+    excerpt: 'This is a test of a test of a test blog.',
+    date: '2022-06-28',
+  },
+  {
+    slug: 'this-is-a-test5',
+    title: 'Test Title',
+    image: 'test-image.png',
+    excerpt: 'This is a test of a test of a test blog.',
+    date: '2022-06-28',
+  },
+];
+
 const HomePage = () => {
   return (
-<Fragment>
-  <Hero />
-  <FeaturedPosts />
-</Fragment>
-  )
-}
+    <Fragment>
+      <Hero />
+      <FeaturedPosts posts={TEST_POSTS} />
+    </Fragment>
+  );
+};
 
-export default HomePage
+export default HomePage;
