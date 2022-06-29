6b1836f1386fbcef609459d0d33ba30b9e648dd2
Max Girteit <icufishmg@gmail.com>
2022-06-29 13:50:07 -0600

    util functions added

---
+++ helpers/posts-util.js
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import path from 'path';
+
+import matter from 'gray-matter';
+
+const postsDir = path.join(process.cwd(), 'posts');
+
+export const getPostData = (fileName) => {
+  const filePath = path.join(postsDir, fileName);
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
+  const { data, content } = matter(fileContent);
+
+  const postSlug = fileName.replace(/\.md$/, ''); // this removes file extension
+
+  const postData = {
+    slug: postSlug,
+    ...data,
+    content,
+  };
+
+  return postData;
+};
+
+export const getAllPosts = () => {
+  const postFiles = fs.readdirSync(postsDir);
+
+  const allPosts = postFiles.map((postFile) => {
+    return getPostData(postFile);
+  });
+
+  const sortedPosts = allPosts.sort((postA, postB) => {
+    postA.date > postB.date ? -1 : 1;
+  });
+
+  return sortedPosts;
+};
+
+export const getFeaturedPosts = () => {
+  const allPosts = getAllPosts();
+  const featuredPosts = allPosts.filter((post) => post.isFeatured);
+  return featuredPosts;
+};
