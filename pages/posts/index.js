c376100ca3815bebaee5881da85384b51dd41a5e
Max Girteit <icufishmg@gmail.com>
2022-06-29 10:47:10 -0600

    allpostspage test data added

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,44 @@
-const AllPostsPage = () => {
-  return (
-    <div>AllPostsPage</div>
-  )
-}
+import AllPosts from '../../components/posts/all-posts';
 
-export default AllPostsPage
+const AllPostsPage = () => {
+  const TEST_POSTS = [
+    {
+      slug: 'this-is-a-test',
+      title: 'Test Title',
+      image: 'test-image.png',
+      excerpt: 'This is a test of a test of a test blog.',
+      date: '2022-06-28',
+    },
+    {
+      slug: 'this-is-a-test2',
+      title: 'Test Title',
+      image: 'test-image.png',
+      excerpt: 'This is a test of a test of a test blog.',
+      date: '2022-06-28',
+    },
+    {
+      slug: 'this-is-a-test3',
+      title: 'Test Title',
+      image: 'test-image.png',
+      excerpt: 'This is a test of a test of a test blog.',
+      date: '2022-06-28',
+    },
+    {
+      slug: 'this-is-a-test4',
+      title: 'Test Title',
+      image: 'test-image.png',
+      excerpt: 'This is a test of a test of a test blog.',
+      date: '2022-06-28',
+    },
+    {
+      slug: 'this-is-a-test5',
+      title: 'Test Title',
+      image: 'test-image.png',
+      excerpt: 'This is a test of a test of a test blog.',
+      date: '2022-06-28',
+    },
+  ];
+  return <AllPosts posts={TEST_POSTS} />;
+};
+
+export default AllPostsPage;
