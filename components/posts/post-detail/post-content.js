d3a20f9e1a6f1703bbb089addfb44784f67bb197
Max Girteit <icufishmg@gmail.com>
2022-06-29 12:08:39 -0600

    styling and react markdown package comp added

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,18 +1,24 @@
+import ReactMarkdown from 'react-markdown';
+
 import PostHeader from './post-header';
+
+import classes from './post-content.module.css';
 
 const TEST__POST = {
   slug: 'this-is-a-test',
   title: 'Test Title',
   image: 'test-image.png',
   date: '2022-06-28',
-  conent: ''
+  content: '# First Blog Post',
 };
 
 const PostContent = () => {
+  const imagePath = `/images/posts/${TEST__POST.slug}/${TEST__POST.image}`;
+
   return (
-    <article>
-      <PostHeader title='' image='' />
-      CONTENT
+    <article className={classes.content}>
+      <PostHeader title={TEST__POST.title} image={imagePath} />
+      <ReactMarkdown>{TEST__POST.content}</ReactMarkdown>
     </article>
   );
 };
