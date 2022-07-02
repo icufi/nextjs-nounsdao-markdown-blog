e8c8ea740b26612bcf3e5c967fb7fe9b34d92a9c
Max Girteit <icufishmg@gmail.com>
2022-07-02 13:19:18 -0600

    added post content, removed test content

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -4,21 +4,14 @@ import PostHeader from './post-header';
 
 import classes from './post-content.module.css';
 
-const TEST__POST = {
-  slug: 'this-is-a-test',
-  title: 'Test Title',
-  image: 'test-image.png',
-  date: '2022-06-28',
-  content: '# First Blog Post',
-};
-
-const PostContent = () => {
-  const imagePath = `/images/posts/${TEST__POST.slug}/${TEST__POST.image}`;
+const PostContent = (props) => {
+  const {post} = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
     <article className={classes.content}>
-      <PostHeader title={TEST__POST.title} image={imagePath} />
-      <ReactMarkdown>{TEST__POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 };
