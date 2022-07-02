e4261667a71a82b8f6f126e96c77eac2e5c29d72
Max Girteit <icufishmg@gmail.com>
2022-07-02 16:54:27 -0600

    database connected with insert functionality

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -18,7 +20,29 @@ function handler(req, res) {
 
     const newMessage = { email, name, message };
 
-    console.log('newMessage', newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        `mongodb+srv://<username>:<password>@cluster0.21ehf.mongodb.net/?retryWrites=true&w=majority`
+      );
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to database.' });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: 'Storing message failed!' });
+      return;
+    }
+
+    client.close();
     res
       .status(201)
       .json({ message: 'Successfully stored message!', message: newMessage });
