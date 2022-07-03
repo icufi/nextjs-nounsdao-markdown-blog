3ba96890d1cc252590edaaf66f5fdbee33be87ff
Max Girteit <icufishmg@gmail.com>
2022-07-03 17:58:15 -0600

    metadata description added

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,22 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
+
 import AllPosts from '../../components/posts/all-posts';
 import { getAllPosts } from '../../helpers/posts-util';
 
 const AllPostsPage = (props) => {
-  return <AllPosts posts={props.posts} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>All Posts</title>
+        <meta
+          name='Description'
+          content='All posts relating to Noun DAO activities in NYC.'
+        />
+      </Head>
+      <AllPosts posts={props.posts} />
+    </Fragment>
+  );
 };
 
 export function getStaticProps() {
