cbcbd56f5e9e22ecccc99804c657f95f8890648e
Max Girteit <icufishmg@gmail.com>
2022-07-03 17:53:09 -0600

    metadata description added

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Fragment } from 'react';
+import Head from 'next/head';
 
 import Hero from '../components/home-page/Hero';
 import FeaturedPosts from '../components/home-page/featured-posts';
@@ -7,6 +8,13 @@ import { getFeaturedPosts } from '../helpers/posts-util';
 const HomePage = (props) => {
   return (
     <Fragment>
+      <Head>
+        <title>Nouns Blog</title>
+        <meta
+          name='description'
+          content='A blog about Noun DAO events at NYC NFT.'
+        /> 
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
