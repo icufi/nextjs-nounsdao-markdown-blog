0286489ab3c1fec27e2d9f8bf676e99dd700167d
Max Girteit <icufishmg@gmail.com>
2022-07-04 16:14:03 -0600

    added env support for dev and production

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
-module.exports = {
-    env: {
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
         mongodb_username: '',
         mongodb_password: '',
         mongodb_clusterName: '',
-        mongodb_database: ''
-    }
+        mongodb_database: '',
+      },
+    };
+  }
+
+  // in production values
+  return {
+    env: {
+      mongodb_username: '',
+      mongodb_password: '',
+      mongodb_clusterName: '',
+      mongodb_database: '',
+    },
+  };
 };
